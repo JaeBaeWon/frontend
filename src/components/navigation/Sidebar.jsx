3579dc0ac31535b8301e33e87d2dedf3e7b7748e
Jin Hyun Jamie Park <jinhyunpark929@gmail.com>
2025-05-15 12:03:47 +0900

    refactor: Header, Footer 컴포넌트 jsx/css 분리 구조개선(#12)

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./Sidebar.module.css";
+import styles from "./Sidebar.css";
 
 const menuList = [
   { label: "내 정보", icon: "👤", link: "/mypage" },
